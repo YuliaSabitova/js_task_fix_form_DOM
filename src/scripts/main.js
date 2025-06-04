5bf0c1e8f25c61e58cdeb63dbf348054b4b6fdff
YuliaSabitova <yuliasab2017@mail.com>
2025-06-04 16:14:35 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,27 @@
 'use strict';
 
-// write code here
+const getInputs = Array.from(document.querySelectorAll('input'));
+
+getInputs.forEach((input) => {
+  const labelElement = document.createElement('label');
+
+  labelElement.classList.add('field-label');
+
+  if (input.name && typeof input.name === 'string' && input.name.length > 0) {
+    const labelText = input.name
+      .replace(/([A-Z])/g, ' $1')
+      .replace(/^./, (char) => char.toUpperCase());
+
+    labelElement.textContent = labelText;
+  }
+  labelElement.setAttribute('for', input.id);
+
+  input.setAttribute(
+    'placeholder',
+    input.name
+      .replace(/([A-Z])/g, ' $1')
+      .replace(/^./, (char) => char.toUpperCase()),
+  );
+
+  input.parentElement.insertBefore(labelElement, input);
+});
